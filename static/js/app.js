2675d17673ee584f52a1054886c66708f70839c8
Saif Gorges <69221324+saif-gorges@users.noreply.github.com>
2021-01-23 20:05:21 -0500

    Added graphs and chart filters

--- static/js/app.js
+++ static/js/app.js
@@ -44,10 +44,67 @@ function optionChanged(selected_id) {
         var otu_labels = result.otu_labels;
         var sample_values = result.sample_values;
 
-        var y_label = otu_ids.slice(0, 10).map(otuID => 'OTU ${otuID}').reverse();
+        var y_label = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
     
-    
-    })
+        console.log("y_label  ");
+        console.log(y_labe);
+
+        console.log('sample_values: ');
+        console.log(sample_values.slice(0, 10).reverse());
+
+        var bar_trace = {
+
+            y: y_label,
+            x: samples_values.slice(0, 10).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: 'h',
+        };
+
+        var data = [bar_trace];
+
+        var bar_layout = {
+
+            title: 'Top 10 OTUs'
+            margin: {t:30, 1:150}
+        };
+
+        Plotly.newPlot('bar', data, bar_layout);
+
+    //create bubble chart
+
+    var results = samples.filter(sampleObj) => sampleObj.id == selected_id;
+    var results = results[0];
+
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+
+    var bubble_trace = {
+
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        market: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale: 'Earth'
+        }
+    };
+
+    var data = [bubble_trace];
+
+    var bubble_layout ={
+
+        hovermode: 'closest',
+        xaxis: { title: 'OTU ID'},
+        margin: {t:30}
+    };
+
+    Plotly.newPlot('bubble', data, bubble_layout);
+
+    });
 
 
 }
